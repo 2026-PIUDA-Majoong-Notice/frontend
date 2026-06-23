c4d1d43553cc1f475aaf75f0622dcfe859dc4a87
sungyeon6 <0504sypark@naver.com>
2026-06-23 19:07:48 +0900

    [FEAT]: 네비바 연결

--- lib/components/bottom_navi_bar.dart
+++ lib/components/bottom_navi_bar.dart
@@ -1,19 +1,46 @@
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
+import '../pages/record_page/record_page_main/record_page_main.dart';
+
+
 class BottomNaviBar extends StatelessWidget {
-  const BottomNaviBar({super.key});
+  final int currentIndex;
+
+  const BottomNaviBar({
+    super.key,
+    required this.currentIndex,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: MediaQuery.of(context).size.height * 0.09,
       child: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          if (index == currentIndex) return;
+
+          if (index == 1) {
+            Get.off(() => const RecordPageMain());
+          }
+        },
         type: BottomNavigationBarType.fixed,
-        backgroundColor: Color(0xffF6FAF8),
-        selectedItemColor: Color(0xff6FAF9B),
-        unselectedItemColor: Color(0xff26332F),
+        backgroundColor: const Color(0xff_F6_FA_F8),
+        selectedItemColor: const Color(0xff6FAF9B),
+        unselectedItemColor: const Color(0xff26332F),
+        selectedLabelStyle: const TextStyle(
+          fontFamily: 'SCDream',
+          fontSize: 10,
+          fontWeight: FontWeight.w400,
+        ),
+        unselectedLabelStyle: const TextStyle(
+          fontFamily: 'SCDream',
+          fontSize: 10,
+          fontWeight: FontWeight.w400,
+        ),
         elevation: 0,
-        items: [
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home_filled),
             label: '홈',
