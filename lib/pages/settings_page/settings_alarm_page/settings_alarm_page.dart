1fb74b1d151bf695284d8f2aa976acc0fd76ba3f
sungyeon6 <0504sypark@naver.com>
2026-06-28 22:00:13 +0900

    [FEAT]: 알림설정 세부 화면 구현

--- lib/pages/settings_page/settings_alarm_page/settings_alarm_page.dart
+++ lib/pages/settings_page/settings_alarm_page/settings_alarm_page.dart
@@ -1,33 +1,60 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/settings_app_bar.dart';
-import '../widgets/settings_menu_tile.dart';
+import 'widgets/settings_alarm_switch_tile.dart';
 
 
-class SettingsAlarmPage extends StatelessWidget {
+class SettingsAlarmPage extends StatefulWidget {
   const SettingsAlarmPage({super.key});
 
   @override
+  State<SettingsAlarmPage> createState() => _SettingsAlarmPageState();
+}
+
+class _SettingsAlarmPageState extends State<SettingsAlarmPage> {
+  bool careTimingAlarm = true;
+  bool missingRecordAlarm = true;
+  bool nightAlarmLimit = true;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xffF8FCFA),
+    return Scaffold(
+      backgroundColor: const Color(0xffF8FCFA),
       body: SafeArea(
         child: Column(
           children: [
-            SettingsAppBar(title: '알림 설정'),
+            const SettingsAppBar(title: '알림 설정'),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 36),
+              padding: const EdgeInsets.symmetric(horizontal: 34),
               child: Column(
                 children: [
-                  SettingsMenuTile(
+                  SettingsAlarmSwitchTile(
                     title: '케어 타이밍 알림',
+                    value: careTimingAlarm,
+                    onChanged: (value) {
+                      setState(() {
+                        careTimingAlarm = value;
+                      });
+                    },
                   ),
-                  SettingsMenuTile(
+                  SettingsAlarmSwitchTile(
                     title: '기록 미입력 알림',
+                    value: missingRecordAlarm,
+                    onChanged: (value) {
+                      setState(() {
+                        missingRecordAlarm = value;
+                      });
+                    },
                   ),
-                  SettingsMenuTile(
+                  SettingsAlarmSwitchTile(
                     title: '야간 알림 제한',
+                    value: nightAlarmLimit,
                     showDivider: false,
+                    onChanged: (value) {
+                      setState(() {
+                        nightAlarmLimit = value;
+                      });
+                    },
                   ),
                 ],
               ),
